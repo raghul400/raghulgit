d5e6eb7a4d378702aa0df460cfb7aaee0d58b156
raghul400 <raghulchandhiran400@gmail.com>
2024-10-09 18:09:13 +0530

    ADoneProject

--- src/org/samplegit/GitSampleClass.java
+++ src/org/samplegit/GitSampleClass.java
@@ -3,6 +3,10 @@ package org.samplegit;
 public class GitSampleClass {
 	public static void main(String[] args) {
 		System.out.println("GitHub has started");
+		String Coursename = "SQL";
+		System.out.println(Coursename);
+		int reg = 5678;
+		System.out.println(reg);
 	}
 
 }
