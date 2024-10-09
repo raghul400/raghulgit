aed9147296d085a51c8a76256a284bea52af8ec8
raghul400 <raghulchandhiran400@gmail.com>
2024-10-09 18:15:30 +0530

    BSelenium

--- src/org/samplegit/GitSampleClass.java
+++ src/org/samplegit/GitSampleClass.java
@@ -3,6 +3,11 @@ package org.samplegit;
 public class GitSampleClass {
 	public static void main(String[] args) {
 		System.out.println("GitHub has started");
+		String CourseDetails = "Python";
+		System.out.println(CourseDetails);
+		
+		int reg = 9876;
+		System.out.println(reg);
 	}
 
 }
